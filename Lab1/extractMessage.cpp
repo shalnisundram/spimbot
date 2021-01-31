8453e44ec7998fafa9875297b2aa6c99c80875fd
Shalni Sundram <shalnis2@linux-a2.ews.illinois.edu>
2021-01-31 14:41:19 -0600

    implement extractMessage()

--- Lab1/extractMessage.cpp
+++ Lab1/extractMessage.cpp
@@ -9,17 +9,34 @@
 
 using namespace std;
 
+int make_mask(int order) {
+    int start = 1;
+    start = start << (order);
+    return start;
+}
+
 unsigned char *extractMessage(const unsigned char *message_in, int length) {
     // length must be a multiple of 8
     assert((length % 8) == 0);
+    
 
     // allocate an array for the output
     unsigned char *message_out = new unsigned char[length];
     for (int i = 0; i < length; i++) {
         message_out[i] = 0;
     }
-
-    // TODO: write your code here
+    
+    for (int byte_group = 0; byte_group < length; byte_group += 8) { // iterate through bytes
+        for (int byte = 0; byte < 8; byte++) { 
+            for (int bit = 0; bit < 8; bit++) {
+                char write_byte = message_out[byte_group + byte]; 
+                char read_byte = message_in[byte_group + bit];    
+                int read_bit = (read_byte & make_mask(byte)) != 0;
+                write_byte = write_byte | (read_bit << bit);
+                message_out[byte_group + byte] = write_byte;
+            }
+        }
+    }
 
     return message_out;
 }
