01e2da048ac48fea0537bc7723f68f3f139eb378
Shalni Sundram <shalnis2@linux-a2.ews.illinois.edu>
2021-02-07 15:54:12 -0600

    implement generator.cpp

--- Lab2/generator.cpp
+++ Lab2/generator.cpp
@@ -3,3 +3,15 @@
 
 // make generator
 // ./generator
+int 
+using namespace std;
+
+main() {
+    int width = 32;
+
+    // alu1
+    for (int i = 1; i < width; i++) {
+        printf(" alu1 a%d(out[%d], c_out[%d], A[%d], B[%d], c_out[%d], control);\n", i, i, i, i, i, i-1);
+    }
+    return 0;
+} 
