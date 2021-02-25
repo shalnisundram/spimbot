ba315772935cd70c7c2b72681d76d5ab70ebda48
Shalni Sundram <shalnis2@linux-a2.ews.illinois.edu>
2021-02-25 15:57:28 -0600

    finish debugging decoder.v

--- Lab5/decoder.v
+++ Lab5/decoder.v
@@ -28,8 +28,8 @@ module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
     output       mem_read, word_we, byte_we, byte_load, slt, lui, addm;
     input  [5:0] opcode, funct;
     input        zero;
-    // bne, beq, j, jr, lui, slt, lw, lbu, sw, sb, addm
-    
+    // // bne, beq, j, jr, lui, slt, lw, lbu, sw, sb, addm
+   // wire op_add, op_sub, op_and, op_or, op_nor, op_xor, op_addi, op_andi, op_ori, op_xori, op_beq, op_bne, op_j, op_jr, op_lui, op_slt, op_lw, op_lbu, op_sw, op_sb, op_addm;
 
     wire addOut = (opcode == `OP_OTHER0) && (funct == `OP0_ADD); // same opcode for all operations without immediate
     wire subOut = (opcode == `OP_OTHER0) && (funct == `OP0_SUB); 
@@ -42,36 +42,38 @@ module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
     wire oriOut = (opcode == `OP_ORI);
     wire xoriOut = (opcode == `OP_XORI);
 
-    wire beq = (opcode == `OP_BEQ);
-    wire bne = (opcode == `OP_BNE);
-    wire j = (opcode == `OP_J);
-    wire jr = (opcode == `OP_OTHER0) && (funct == `OP0_JR);
-    wire lui = (opcode == `OP_LUI);
-    wire slt = (opcode == `OP_OTHER0) && (funct == `OP0_SLT); 
-    wire lw = (opcode == `OP_LW);
-    wire lbu = (opcode == `OP_LBU);
-    wire sw = (opcode == `OP_SW);
-    wire sb = (opcode == `OP_SB);
-    wire addm = (opcode == `OP_OTHER0) && (funct == `OP0_ADDM);
+    wire beqOut = (opcode == `OP_BEQ);
+    wire bneOut = (opcode == `OP_BNE);
+    wire jOut = (opcode == `OP_J);
+    wire jrOut = (opcode == `OP_OTHER0) && (funct == `OP0_JR);
+    wire luiOut = (opcode == `OP_LUI);
+    wire sltOut = (opcode == `OP_OTHER0) && (funct == `OP0_SLT); 
+    wire lwOut = (opcode == `OP_LW);
+    wire lbuOut = (opcode == `OP_LBU);
+    wire swOut = (opcode == `OP_SW);
+    wire sbOut = (opcode == `OP_SB);
+    wire addmOut = (opcode == `OP_OTHER0) && (funct == `OP0_ADDM);
     
-    assign rd_src = (addiOut | andiOut | oriOut | xoriOut | lui | lw | lbu | sb | sw);
-    //assign writeenable = (addOut | subOut | andOut | orOut | norOut | xorOut | addiOut | andiOut | oriOut | xoriOut | lui | slt | lw | lbu);
-    assign writeenable = ~(beq | bne | j | jr | sw | sb |except);
-    assign alu_src2[0] = (addiOut | lw | lbu | sw | sb);
-    assign alu_src2[1] = (oriOut | andiOut | xoriOut);
-    assign alu_op[0] = (subOut | orOut | xorOut | oriOut | xoriOut | beq | bne | slt);
-    assign alu_op[1] = (addOut | subOut | norOut | xorOut | addiOut | xoriOut | beq | bne | sw | lw | lbu | slt | sb | addm);
+    assign alu_op[0] = (subOut | orOut | xorOut | oriOut | xoriOut | beqOut | bneOut | sltOut);
+    assign alu_op[1] = (addOut | subOut | norOut | xorOut | addiOut | xoriOut | beqOut | bneOut | swOut | lwOut | lbuOut | sltOut | sbOut | addmOut);
     assign alu_op[2] = (andOut | orOut | norOut | xorOut | andiOut | oriOut | xoriOut);
-    assign except = ~(addOut | subOut | andOut | orOut | norOut | xorOut | addiOut | andiOut | oriOut | xoriOut | bne | beq | j |jr | lui | slt | lw | lbu | sw | sb | addm);
+    assign rd_src = (addiOut | andiOut | oriOut | xoriOut | luiOut | lwOut| lbuOut | sbOut | swOut);
+
+    assign except = ~(addOut | subOut | andOut | orOut | norOut | xorOut | addiOut | andiOut | oriOut | xoriOut | bneOut | beqOut | jOut | jrOut | luiOut | sltOut | lwOut | lbuOut | swOut | sbOut | addmOut);
     
-    assign control_type[0] = (beq & zero | (bne & ~zero)| jr);
-    assign control_type[1] = (j | jr);
-    assign mem_read = (lw | lbu);
-    assign word_we = sw;
-    assign byte_we = sb;
-    assign byte_load = lbu;
-    assign lui = lui;
-    assign slt = slt;
-    assign addm = addm; 
+    assign writeenable = (addOut | subOut | andOut | orOut | norOut | xorOut | addiOut | andiOut | oriOut | xoriOut | luiOut | sltOut | lwOut | lbuOut);
+    assign alu_src2[0] = (addiOut | lwOut | lbuOut | swOut | sbOut);
+    assign alu_src2[1] = (oriOut | andiOut | xoriOut);
+    //assign alu_src2 = (addiOut | andiOut | oriOut | xoriOut | luiOut | lwOut | lbuOut | swOut | sbOut);
+
+    assign control_type[0] = (beqOut & zero | (bneOut & ~zero)| jrOut);
+    assign control_type[1] = (jOut | jrOut);
+    assign mem_read = (lwOut | lbuOut);
+    assign word_we = swOut;
+    assign byte_we = sbOut;
+    assign byte_load = lbuOut;
+    assign lui = luiOut;
+    assign slt = sltOut;
+    assign addm = addmOut; 
 
 endmodule // mips_decode
