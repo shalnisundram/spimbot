f26bb36e38e294e2b7cc2c4c442cb16e35452392
Shalni Sundram <shalnis2@linux-a2.ews.illinois.edu>
2021-03-04 17:25:40 -0600

    implement first draft of p1.s

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -36,5 +36,57 @@
 # }
 
 .globl craftable_recipes
+mips_asm:
+        
 craftable_recipes:
-        jr      $ra
+        li $t0, 5               # $t0 holds NUM_ITEMS (NUM COLUMNS)
+        li $t1, 10              # $t1 holds NUM_RECIPES (NUM ROWS)
+        li $t2, 0               # $t2 holds recipe_index (i or row)
+
+        # **check if dealing with array params correctly**
+
+        for_one: 
+                bge $t2, $t1, out
+                mul $t3, $t2, 4     # $t3 has offset 4
+                add $t4, $a2, $t3   # &times_craftable[r_ind]  **NOTE: check what to do for times_craftable[10]**     
+                li $s0, 0x7fffffff  # CLARIFY THIS LI: $t4 - times_craftable[recipe_index] = 0x7fffffff
+                sw $s0, 0($t4)
+                li $t9, 0            # assigned = 0
+                
+                li $t5, 0           # $t5 holds item_index (col)
+                for_two:
+                        bge $t5, $t0, check_assign
+                        if:                      # 2D array indexing: array[row * NUM COLUMNS + col]
+                            mul $t6, $t2, $t0    # need to do a t2 offset?
+                            add $t6, $t6, $t5   
+                            mul $t6, $t6, 4      # col offset
+                            add $t7, $a1, $t6    
+                            lw $t7, 0($t7)       # $t7 holds recipes[recipe_idx][item_idx]
+                            
+                            ble $t7, $zero, after_if 
+                            mul $t8, $t5, 4            
+                            add $t8, $a0, $t8
+                            lw $t8, 0($t8)  
+                            div $t8, $t8, $t7    # $t8 now holds times_item_req
+
+                            if_two:
+                                bge $t8, $s0, after_if
+                                sw $t8, 0($t4)  
+                                li $t9, 1        # assigned = 1 
+                                # add $t7, $t7, 1  # increment col
+                                # j for_two
+
+                        after_if:
+                                add $t5, $t5, 1  # increment column
+                                j for_two
+                
+                check_assign:
+                        bne $t9, $zero, after
+                        sw $zero, 0($t4)
+
+                after:
+                        add $t2, $t2, 1
+                        j for_one
+
+        out:
+                jr      $ra
