d9f35fbde215e47a934574c245aab019ab8b0eca
Shalni Sundram <shalnis2@linux-a2.ews.illinois.edu>
2021-04-28 16:05:44 -0500

    vectorize mv_mutl_vector() using SIMD

--- Lab13/mv-mult.cpp
+++ Lab13/mv-mult.cpp
@@ -14,14 +14,38 @@
 // vectorize the below code using SIMD intrinsics
 float *
 mv_mult_vector(float mat[SIZE][SIZE], float vec[SIZE]) {
-    static float ret[SIZE];
+    // static float ret[SIZE];
 
-    for (int i = 0; i < SIZE; i ++) {
+    // for (int i = 0; i < SIZE; i ++) {
+    //     ret[i] = 0;
+    //     for (int j = 0; j < SIZE; j ++) {
+    //         ret[i] += mat[i][j] * vec[j];
+    //     }
+    // }
+
+    // return ret;
+
+    static float ret[SIZE];
+    float temp[4];
+    __m128 mat_index, vec_index, final_sum;
+    final_sum = _mm_set1_ps(0.0); // set all four words in final_sum to 0.0
+
+    for (int i = 0; i < SIZE; i++) {
         ret[i] = 0;
-        for (int j = 0; j < SIZE; j ++) {
-            ret[i] += mat[i][j] * vec[j];
+        int j = 0;
+
+        final_sum = _mm_set1_ps(0.0);
+        for (; j < (SIZE - 3); j += 4) {
+            mat_index = _mm_loadu_ps(&mat[i][j]);
+            vec_index = _mm_loadu_ps(&vec[j]);
+            final_sum = _mm_add_ps(final_sum, _mm_mul_ps(mat_index, vec_index));
+        }
+        _mm_store_ps(temp, final_sum);
+        ret[i] += temp[0] + temp[1] + temp[2] + temp[3];
+
+        for (; i < SIZE; i++) {
+            ret[i] += mat[i][j] + vec[j];
         }
     }
-
     return ret;
-}
+} 
